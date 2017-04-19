c9832f6a9892de1bb6a3462d3573323a64a52f34
William Pickering <willpicky@gmail.com>
2017-04-19 17:48:49 +0100

    App working with own api #yay

--- src/components/Barcode.js
+++ src/components/Barcode.js
@@ -7,7 +7,7 @@ import Prompt from 'react-native-prompt';
 
 const cameraIcon = (<Icon name="camera" size={30} />)
 
-const ROOT = 'https://world.openfoodfacts.org/api/v0/product/';
+const ROOT = 'https://gentle-escarpment-88131.herokuapp.com/api/products';
 const POSTROOT = 'https://world.openfoodfacts.org/cgi/product_jqm2.pl';
 
 export default class Barcode extends Component {
@@ -31,10 +31,12 @@ function getName (EAN) {
   axios
       .get(`${ROOT}/${EAN}`)
       .then(function scanBarcode (data) {
-        alert(`${data.data.product.product_name} (${EAN}) Packaging: ${data.data.product.packaging}`);
+        console.log(data.data.products[0].name);
+        alert(`${data.data.products[0].name} (${EAN}) Packaging: ${data.data.products[0].packaging}`);
       })
       .catch(function (error) {
-        axios.get(`${POSTROOT}?code=${EAN}&product_name=Test`).then((data) => {alert(`Product ${EAN} added as Test`)}).catch((err) => alert(err));
+        alert(error);
+        // axios.get(`${POSTROOT}?code=${EAN}&product_name=Test`).then((data) => {alert(`Product ${EAN} added as Test`)}).catch((err) => alert(err));
       });
 }
 
